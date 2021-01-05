e95d24b3fb6e10c5cd8f87e8b1c3bcd46b525afc
mahinour30 <mahinour1233@gmail.com>
2021-01-06 01:59:46 +0200

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 
-const Home = () => {
+
+const Home = ({navigation}) => {
   return (
     <View>
-      <Text>
-        Home.js
-      </Text>
+      <Button
+        title="Go to SplashScreen... again"
+        onPress={() => navigation.navigate('SplashScreen')}
+        />
+        <Button
+        title="Go to Onboarding1... again"
+        onPress={() => navigation.navigate('Onboarding1')}
+        />
+        <Button
+        title="Go to Onboarding2... again"
+        onPress={() => navigation.navigate('Onboarding2')}
+        />
+        <Button
+        title="Go to Onboarding3... again"
+        onPress={() => navigation.navigate('Onboarding3')}
+        />
+        <Button
+        title="Go to Menu... again"
+        onPress={() => navigation.navigate('Menu')}
+        />
+        <Button
+        title="Go to Order... again"
+        onPress={() => navigation.navigate('Order')}
+        />
     </View>
   );
 };

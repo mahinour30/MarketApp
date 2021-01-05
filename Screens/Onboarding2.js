f13028a58de62c9ad65ddaaa18dd88beb45c9e67
mahinour30 <mahinour1233@gmail.com>
2021-01-05 23:19:55 +0200

    Create Onboarding2.js

---
+++ Screens/Onboarding2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const Onboarding2 = () => {
+  return (
+    <View>
+      <Text>
+        Onboarding2.js
+      </Text>
+    </View>
+  );
+};
+
+export default Onboarding2;

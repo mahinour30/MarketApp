ebd5bda749e3c14cd8df01317b2d36f3da7fd86f
mahinour30 <mahinour1233@gmail.com>
2021-01-05 23:20:02 +0200

    Create SplashScreen.js

---
+++ Screens/SplashScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>
+        SplashScreen.js
+      </Text>
+    </View>
+  );
+};
+
+export default SplashScreen;

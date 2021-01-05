fd23a944e1aba7207337e64674bd9864cc771e1f
mahinour30 <mahinour1233@gmail.com>
2021-01-05 23:19:57 +0200

    Create Onboarding3.js

---
+++ Screens/Onboarding3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const Onboarding3 = () => {
+  return (
+    <View>
+      <Text>
+        Onboarding3.js
+      </Text>
+    </View>
+  );
+};
+
+export default Onboarding3;

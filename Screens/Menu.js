812d950815a40cd7eaa354880df6a8e925008622
mahinour30 <mahinour1233@gmail.com>
2021-01-05 23:19:28 +0200

    Create Menu.js

---
+++ Screens/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const Menu = () => {
+  return (
+    <View>
+      <Text>
+        Menu.js
+      </Text>
+    </View>
+  );
+};
+
+export default Menu;

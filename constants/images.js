69fbda83d1a0a85fcf7f73ea984b8384ab3b52cc
mahinour30 <mahinour1233@gmail.com>
2021-01-06 21:42:22 +0200

    Create images.js

---
+++ constants/images.js
@@ -0,0 +1,25 @@
+const Car = require('../assets/images/Car.png');
+const Cart = require('../assets/images/Cart.png');
+const FM = require('../assets/images/FM.png');
+const Fruit1 = require('../assets/images/Fruit1.png');
+const Fruit2 = require('../assets/images/Fruit2.png');
+const Fruit3 = require('../assets/images/Fruit3.png');
+const Like = require('../assets/images/Like.png');
+const Meat = require('../assets/images/Car.png');
+const Pasta = require('../assets/images/Pasta.png');
+const Veg1 = require('../assets/images/Veg1.png');
+const Veg2 = require('../assets/images/Veg2.png');
+
+export default{
+    Car,
+    Cart,
+    FM,
+    Fruit1,
+    Fruit2,
+    Fruit3,
+    Like,
+    Meat,
+    Pasta,
+    Veg1,
+    Veg2,
+}

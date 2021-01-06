eef4dad42c50d635527e9e1c32454640bcd64263
mahinour30 <mahinour1233@gmail.com>
2021-01-06 21:42:20 +0200

    Create icons.js

---
+++ constants/icons.js
@@ -0,0 +1,21 @@
+export const Add = require('../assets/icons/Add.png');
+export const Done = require('../assets/icons/Done.png');
+export const Left = require('../assets/icons/Left.png');
+export const ListIcon = require('../assets/icons/ListIcon.png');
+export const Right = require('../assets/icons/Right.png');
+export const SearchIcon1 = require('../assets/icons/SearchIcon1.png');
+export const SearchIcon2 = require('../assets/icons/SearchIcon2.png');
+export const ShopingCartIcon1 = require('../assets/icons/ShopingCartIcon1.png');
+export const ShopingCartIcon2 = require('../assets/icons/ShopingCartIcon2.png');
+
+export default{
+    Add,
+    Done,
+    Left,
+    ListIcon,
+    Right,
+    SearchIcon1,
+    SearchIcon2,
+    ShopingCartIcon1,
+    ShopingCartIcon2,
+}

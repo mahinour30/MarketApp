0cbffa22df502e9f440e695b05cf117d7881a22c
mahinour30 <mahinour1233@gmail.com>
2021-01-07 02:06:21 +0200

    Update SplashScreen.js

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Image} from 'react-native';
 import { images, COLORS } from '../constants'
 
-const SplashScreen = () => {
+const SplashScreen = ({navigation}) => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Onboarding1')
+    }, 2000);
+  });
+  
   return (
     <View style ={{ flex:1, backgroundColor :COLORS.primary}}>
       <View style ={{ flexDirection:'row', flex:1, alignSelf:'center'}}>

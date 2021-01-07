c0ebcbc9ab5a7ade4f821e7c5861d0d9184b9a4f
mahinour30 <mahinour1233@gmail.com>
2021-01-08 01:01:00 +0200

    Update Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,35 +1,102 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button, ScrollView, FlatList, Image, ImageBackground} from 'react-native';
+import {COLORS, FONTS, SIZES, images, icons} from '../constants'
 
+const URL = 'https://5bcce576cf2e850013874767.mockapi.io/task/categories';
 
 
 const Home = ({navigation}) => {
+
+  const [data, setData]=useState([]);
+
+  useEffect(()=>{
+    fetch(URL)
+    .then((response)=> response.json())
+    .then((json)=> {
+      setData(json)
+    })
+    .catch((error) => Alert.alert('Error','Something IS Wrong!'))
+  }, []);
+
+
+  console.log("from home ===>", data)
+
   return (
-    <View>
-      <Button
-        title="Go to SplashScreen... again"
-        onPress={() => navigation.navigate('SplashScreen')}
-        />
-        <Button
-        title="Go to Onboarding1... again"
-        onPress={() => navigation.navigate('Onboarding1')}
-        />
-        <Button
-        title="Go to Onboarding2... again"
-        onPress={() => navigation.navigate('Onboarding2')}
-        />
-        <Button
-        title="Go to Onboarding3... again"
-        onPress={() => navigation.navigate('Onboarding3')}
-        />
-        <Button
-        title="Go to Menu... again"
-        onPress={() => navigation.navigate('Menu')}
-        />
-        <Button
-        title="Go to Order... again"
-        onPress={() => navigation.navigate('Order')}
-        />
+    <View style={{flex:1, backgroundColor:COLORS.white}}>
+     <View style={{backgroundColor:'white', width:'100%', height:SIZES.height*0.08, flexDirection:'row'}}>
+       <Image
+       source={icons.ListIcon}
+       resizeMode='contain'
+       style={{width:28, height:14, paddingLeft:95,marginTop:20, marginRight:220}}
+       />
+        <Image
+       source={icons.SearchIcon1}
+       resizeMode='contain'
+       style={{width:40, height:20, marginTop:20}}
+       />
+        <Image
+       source={icons.ShopingCartIcon1}
+       resizeMode='contain'
+       style={{width:40, height:20, marginTop:20}}
+       />
+     </View>
+
+<ScrollView>
+<View>
+       <ImageBackground
+       style={{width:'100%', height:SIZES.height*0.33}}
+       source={images.Veg2}
+       resizeMode='cover'
+       >
+         <View>
+         <Text style={{...FONTS.h1, color:COLORS.white, paddingTop:55,paddingLeft:50}}>
+              20%
+            </Text>
+            <Text style={{...FONTS.h2, color:COLORS.white, paddingLeft:60}}>
+              Discount
+            </Text>
+         </View>
+            
+       </ImageBackground>
+     </View>
+
+
+     <View style={{flexDirection:'row', paddingVertical:5, paddingHorizontal:20}}>
+     <Image
+       style={{width:'50%', height:SIZES.height*0.26}}
+       source={images.Veg1}
+       resizeMode='contain'
+       />
+       <Image
+       style={{width:'50%', height:SIZES.height*0.26}}
+       source={images.Pasta}
+       resizeMode='contain'
+       />
+     </View>
+
+     <View>
+       <Image
+       style={{width:'90%', height:SIZES.height*0.16,alignSelf:'center'}}
+       source={images.Fruit3}
+       resizeMode='contain'
+       />
+     </View>
+
+     <View style={{flexDirection:'row', paddingVertical:5, paddingHorizontal:20}}>
+     <Image
+       style={{width:'50%', height:SIZES.height*0.26}}
+       source={images.Fruit2}
+       resizeMode='contain'
+       />
+       <Image
+       style={{width:'50%', height:SIZES.height*0.26}}
+       source={images.Fruit1}
+       resizeMode='contain'
+       />
+     </View>
+
+  </ScrollView>     
+     
     </View>
   );
 };

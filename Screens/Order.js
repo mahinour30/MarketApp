972d79be03fcd3a8c1fd76fe77986f2649f1bf87
mahinour30 <mahinour1233@gmail.com>
2021-01-10 23:27:49 +0200

    Update Order.js

--- Screens/Order.js
+++ Screens/Order.js
@@ -3,6 +3,19 @@ import {View, Text} from 'react-native';
 
 
 const Order = () => {
+
+  const [data, setData]=useState([]);
+
+  useEffect(()=>{
+    fetch(URL)
+    .then((response)=> response.json())
+    .then((json)=> {
+      setData(json)
+    })
+    .catch((error) => Alert.alert('Error','Something IS Wrong!'))
+  }, []);
+
+  
   return (
     <View>
       <Text>
